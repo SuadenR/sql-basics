de639a4fea04d73df9649fdc8b4ce2728ab81e53
Suaden.Rowland <suadenrowland@gmail.com>
2023-10-09 14:51:03 -0700

    ARTISTSQL

---
+++ sql-basics/artist.sql
@@ -0,0 +1,22 @@
+--PROBLEM 1
+
+INSERT INTO artist (name)
+VALUES('EMINEM'),
+('BEYONCE'),
+('MARIAH CAREY');
+
+--PROBLEM 2
+
+SELECT * FROM artist ORDER BY name DESC LIMIT 10;
+
+--PROBLEM 3
+
+SELECT * FROM artist ORDER BY name ASC LIMIT 5;
+
+--PROBLEM 4
+
+SELECT * FROM artist WHERE name LIKE 'Black%';
+
+--PROBLEM 5
+
+SELECT * FROM artist WHERE name LIKE '%Black%';
